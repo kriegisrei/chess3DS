7795656e18245ddfed01ce2b7160caca39089407
ItsPizzaTime1501 <49124932+ItsPizzaTime1501@users.noreply.github.com>
2020-07-31 23:33:22 -0400

    free

--- source/main.c
+++ source/main.c
@@ -2,31 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <3ds.h>
-
 int main(int argc, char* argv[])
 {
-	u32 aptbuf[0x400/4];
 	gfxInitDefault();
 	consoleInit(GFX_TOP, NULL);
-	printf("plac is not gae\n");
+	printf("plac gae");
+	printf("I don't like this");
+	printf("Pretend this is chess, ok?");
+	printf("plac gae\n");
 	printf("I don't like this\n");
 	printf("Pretend this is chess, ok?\n");
-	printf("Press START to exit. Press B for FRII GAM");
 	// Main loop
 	while (aptMainLoop())
 	{
 		gspWaitForVBlank();
 		gfxSwapBuffers();
 		hidScanInput();
-
 		u32 kDown = hidKeysDown();
-		if (kDown & KEY_B) {
-			aptLaunchLibraryApplet(APPID_MINT, aptbuf, sizeof(aptbuf), 0);
-		}
-		else if (kDown & KEY_START)
+		if (kDown & KEY_START)
 			break; // break in order to return to hbmenu
 	}
-
 	gfxExit();
 	return 0;
 }
